f09de8a66889be80585d4d9199a7d0a5014230a5
xctang <xctang09@gmail.com>
2025-04-05 21:19:23 -0500

    Add files via upload

---
+++ Lecture03/flashingLED.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO   #import GPIO library
+import time
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(2, 1)     #turn on the LED
+        time.sleep(1)         #wait for 1 sec
+        GPIO.output(2, 0)     #turn off the LED
+        time.sleep(1)         #wait for 1 sec
+except KeyboardInterrupt:
+    GPIO.cleanup()
+        
